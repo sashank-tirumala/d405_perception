439c1a565635dad36ca6f28fea28812cd779019f
sashank-tirumala <stsashank6@gmail.com>
2022-04-18 12:55:35 -0400

    First commit

---
+++ src/d405.py
@@ -0,0 +1,40 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+if(__name__ == "__main__"):
+# Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+    pipeline.start(config)
+
+    try:
+        while True:
+
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            
+
+    finally:
+
+        # Stop streaming
+        pipeline.stop()
+
